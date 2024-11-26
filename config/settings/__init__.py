d5cb7dff06c907e896ef78a3ead436ed20a8858b
bekjon <bekjonabdurasulov21@gmail.com>
2024-11-26 19:04:50 +0500

    templates add

---
+++ config/settings/__init__.py
@@ -0,0 +1,4 @@
+from .base import *
+from .auth import *
+from .database import *
+from .templates import *
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.general.views import home,products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('products/', products, name='products'),
 ]
